53ab6ce70da4b83c8245a25e745a3f0bd94282bd
Jannik Bertram <jb@allgravy.com>
2024-09-12 14:25:23 +0200

    Add Sentry integration for error tracking

---
+++ src/instrument.ts
@@ -0,0 +1,13 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+import * as Sentry from "@sentry/node";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+
+Sentry.init({
+  dsn: "https://fa26b75a35ab25001f37a38b2ef4acd7@o4507939297820672.ingest.de.sentry.io/4507939302080592",
+  integrations: [nodeProfilingIntegration()],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
--- src/setup/installation.created.ts
+++ src/setup/installation.created.ts
@@ -1,6 +1,7 @@
 import { Probot, ProbotOctokit } from "probot";
-import { getDefaultBranch } from "../github/github";
+
 import { APP_NAME, defaultConfigYaml } from "../config/config";
+import { getDefaultBranch } from "../github/github";
 import { findFluentResourceFile } from "./translation_file_finder";
 
 export const INITIAL_BRANCH_NAME = `${APP_NAME}/config`;
@@ -14,63 +15,53 @@ export const createInitialPR = async (
 ) => {
   const commitMessage = `Add ${APP_NAME} configuration file`;
 
-  try {
-    const defaultBranch = await getDefaultBranch(octokit, owner, repo);
+  const defaultBranch = await getDefaultBranch(octokit, owner, repo);
 
-    // Create a new branch from the default branch
-    const { data: refData } = await octokit.git.getRef({
-      owner: owner,
-      repo,
-      ref: `heads/${defaultBranch}`,
-    });
+  // Create a new branch from the default branch
+  const { data: refData } = await octokit.git.getRef({
+    owner: owner,
+    repo,
+    ref: `heads/${defaultBranch}`,
+  });
 
-    await octokit.git.createRef({
-      owner,
-      repo,
-      ref: `refs/heads/${INITIAL_BRANCH_NAME}`,
-      sha: refData.object.sha,
-    });
+  await octokit.git.createRef({
+    owner,
+    repo,
+    ref: `refs/heads/${INITIAL_BRANCH_NAME}`,
+    sha: refData.object.sha,
+  });
 
-    const baseFilePath = await findFluentResourceFile(
-      app,
-      octokit,
-      owner,
-      repo
+  const baseFilePath = await findFluentResourceFile(app, octokit, owner, repo);
+
+  if (baseFilePath) {
+    app.log.info(
+      `Found FluentResource file at ${baseFilePath} in ${owner}/${repo}`
     );
-
-    if (baseFilePath) {
-      app.log.info(
-        `Found FluentResource file at ${baseFilePath} in ${owner}/${repo}`
-      );
-    } else {
-      app.log.info(`No FluentResource file found in ${owner}/${repo}`);
-    }
-
-    // Create the configuration file in the new branch
-    await octokit.repos.createOrUpdateFileContents({
-      owner: owner,
-      repo,
-      path: CONFIG_FILE_PATH,
-      message: commitMessage,
-      content: defaultConfigYaml(baseFilePath, "base64"),
-      branch: INITIAL_BRANCH_NAME,
-    });
-
-    // Create a Pull Request to merge the new configuration
-    await octokit.pulls.create({
-      owner,
-      repo,
-      title: "Add default configuration file",
-      head: INITIAL_BRANCH_NAME,
-      base: defaultBranch,
-      body:
-        "This PR adds a default configuration file for Translatabot\n" +
-        "Make sure to update 'defaultPath' and 'languages' according to your needs.",
-    });
-
-    app.log.info(`PR created successfully in ${owner}/${repo}`);
-  } catch (error) {
-    const errorMessage = error instanceof Error ? error.message : error;
-    app.log.error(`Failed to create PR in ${owner}/${repo}: ${errorMessage}`);
+  } else {
+    app.log.info(`No FluentResource file found in ${owner}/${repo}`);
   }
+
+  // Create the configuration file in the new branch
+  await octokit.repos.createOrUpdateFileContents({
+    owner: owner,
+    repo,
+    path: CONFIG_FILE_PATH,
+    message: commitMessage,
+    content: defaultConfigYaml(baseFilePath, "base64"),
+    branch: INITIAL_BRANCH_NAME,
+  });
+
+  // Create a Pull Request to merge the new configuration
+  await octokit.pulls.create({
+    owner,
+    repo,
+    title: "Add default configuration file",
+    head: INITIAL_BRANCH_NAME,
+    base: defaultBranch,
+    body:
+      "This PR adds a default configuration file for Translatabot\n" +
+      "Make sure to update 'defaultPath' and 'languages' according to your needs.",
+  });
+
+  app.log.info(`Created PR to add config file in ${owner}/${repo}`);
 };
