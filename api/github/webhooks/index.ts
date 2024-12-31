d8863d5568bba24a32809a5eb1eba720d1bba34b
jannikb <jannik.bertram94@gmail.com>
2024-12-31 13:20:18 +0100

    Run app as standalone server (#52)

--- api/github/webhooks/index.ts
+++
@@ -1,8 +0,0 @@
-import { createNodeMiddleware, createProbot } from "probot";
-
-import { App } from "../../../src/index";
-
-export default createNodeMiddleware(App, {
-  probot: createProbot(),
-  webhooksPath: "/api/github/webhooks",
-});
